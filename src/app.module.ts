74e17f9d780cd9134d9e01a6c82e825b5958ebc7
nnminh-haise <nnminh.sam.1803@gmail.com>
2025-06-05 20:13:44 +0700

    update: init payment module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppConfigModule } from 'src/configs';
 import { SampleModule } from './modules/sample/sample.module';
+import { PaymentModule } from './modules/payment/payment.module';
 
 @Module({
-  imports: [AppConfigModule, SampleModule],
+  imports: [AppConfigModule, SampleModule, PaymentModule],
   controllers: [],
   providers: [],
 })
